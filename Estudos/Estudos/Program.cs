04fc864aec130a9e5a38ff48c3a099bfdee2935b
Juan Savedra <juansavedra827@gmail.com>
2022-04-29 12:02:51 -0300

    program structure

---
+++ Estudos/Estudos/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; //Namespace padrão do dotnet para funções padrões
+
+/* Namespace 
+Agrupamento de classes e programas
+*/
+
+namespace Program //Namespace Criado por Você (Questão de boa prática, não é algo obrigatório)
+{
+    class Program //Classe
+    {
+        static void Main(string[] args) //Membro da classe. (string[] args = entryPoint 'primeira execução' da aplicação)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
