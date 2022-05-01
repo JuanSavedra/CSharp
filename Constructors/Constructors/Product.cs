73b3a4826662b5d0ca245c8689a844702782ce22
Juan Savedra <juansavedra827@gmail.com>
2022-05-01 13:19:55 -0300

    constructors

---
+++ Constructors/Constructors/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors {
+    class Product {
+        public string name;
+        public double price;
+        public int quantity;
+
+        public Product(string name, double price, int quantity) { //Faz os valores não serem mais nulos
+            this.name = name;
+            this.price = price;
+            this.quantity = quantity;
+        }
+    }
+}
---
+++ Constructors/Constructors/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Constructors {
+    class Program {
+        static void Main(string[] args) {
+            Constructors();
+        }
+
+        static void Constructors() {
+            /* Construtores 
+            É uma operação especial da classe, que executa no momento da instanciação do objeto
+            Se um construtor customizado não for especificado, a classe disponibiliza o construtor 
+            padrão (Product p = new Product())
+            É possível especificar mais de um construtor na mesma classe (sobrecarga)
+            */
+
+            /* Product product = new Product("TV", 900.00, 10); //Produto instanciado com dados padrão */
+
+            Console.WriteLine("Entre com os dados do produto: ");
+            Console.Write("Nome: ");
+            string name = Console.ReadLine(); //Variável do nome para ser adicionado ao construtor 
+            Console.Write("Preço: ");
+            double price = double.Parse(Console.ReadLine());
+            Console.Write("Quantidade em estoque: ");
+            int quantity = int.Parse(Console.ReadLine());
+
+            Product product = new Product(name, price, quantity);
+        }
+    }
+}
