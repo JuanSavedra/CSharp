5c84d84fe8d57f5b8e08d4ebd6bbf07b6f8122c0
Juan Savedra <juansavedra827@gmail.com>
2022-05-01 19:47:15 -0300

    exercise 1

---
+++ Constructors/Constructors/Bank.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Constructors {
+    class Bank {
+        private string _name;
+
+        public int id { get; private set; }
+        public double balance { get; private set; }
+
+        public Bank(string name, int id) {
+            _name = name;
+            this.id = id;
+        }
+
+        public string Name {
+            get { return _name; }
+            set {
+                if (value != null && value.Length > 2) {
+                    _name = value;
+                }
+            }
+        }
+
+        public int ID {
+            get { return id; }
+            set {
+                id = value;
+            }
+        }
+
+        public double Deposit {
+            get { return balance; }
+            set {
+                balance += value;
+            }
+        }
+
+        public double Withdraw {
+            get { return balance; }
+            set {
+                if (value + 5.00 > balance) {
+                    string response = null;
+                    Console.WriteLine("Seu saldo irá ficar negativo, deseja prosseguir? (s/n)");
+                    response = Console.ReadLine();
+                    if (response == "s") {
+                        balance -= value;
+                        balance -= 5.00;
+                    }
+                    else if (response == "n") {
+                        Console.WriteLine("Você cancelou a transação.");
+                    }
+                    else {
+                        Console.WriteLine("Erro...");
+                    }
+                }
+                else {
+                    balance -= value;
+                    balance -= 5.00;
+                }
+            }
+        }
+
+        public override string ToString() {
+            return $"\n" +
+                $"Conta: {id}, " +
+                $"Titular: {_name}, " +
+                $"Saldo: {balance.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
--- Constructors/Constructors/Program.cs
+++ Constructors/Constructors/Program.cs
@@ -11,7 +11,8 @@ namespace Constructors {
             //Encapsulation();
             //Properties();
             //AutoProperties();
-            AccessModifiers();
+            //AccessModifiers();
+            Exercise1();
         }
 
         static void Constructors() {
@@ -137,7 +138,7 @@ namespace Constructors {
             Console.WriteLine($"Novo nome do produto: {product.Name}");
             Console.WriteLine($"Dados gerais do produto: {product}");
         }
-        
+
         static void AccessModifiers() {
             /* Modificadores de Acesso 
              * 1 - Própria classe
@@ -152,6 +153,64 @@ namespace Constructors {
             private protected - [1][2]
             private - [1]
             */
+        }
+
+        static void Exercise1() {
+            /* 
+            Em um banco, para se cadastrar uma conta bancária, é necessário informar o
+            número da conta, o nome do titular da conta, e o valor do depósito inicial
+            que o titular depositou ao abrir a conta. Este valor de depósito inicial,
+            entretanto, é opcional, ou seja: Se o titular não tiver dinheiro a depositar
+            no momento de abrir sua conta, o depósito inicial não será feito e o saldo 
+            inicial da conta será, naturalmente zero.
+            Importante: uma vez que uma conta bancária foi aberta, o número da conta nunca 
+            poderá ser alterado. Já o nome do titular pode.
+            Por fim, o saldo da conta não pode ser alterado livremente. É preciso haver um 
+            mecanismo para proteger isso. O saldo só aumenta por meio de depósitos, e só 
+            diminui por meio de saques. Para cada saque realizado, o banco cobra uma taxa 
+            de R$5,00. Nota: A conta pode ficar com saldo negativo se o saldo não for 
+            suficiente para realizar o saque e/ou pagar a taxa.
+            Você deve informar um programa que realize o cadastro de uma conta, dando opção
+            para que seja ou não informado o valor de depósito inicial. Em seguida, realizar 
+            um depósito e depois um saque, sempre mostrando os dados da conta após cada 
+            operação.
+            */
+
+            string name; 
+            string response = null;
+            Bank bank = new Bank("Desconhecido", 0000);
+
+            Console.Write("Entre com o número da conta: ");
+            bank.ID = int.Parse(Console.ReadLine());
+            Console.Write("Diga o nome do titular da conta: ");
+            bank.Name = Console.ReadLine();
+            Console.Write("Haverá depósito inicial? (s/n)");
+            response = Console.ReadLine();
+
+            if (response == "s") {
+                Console.Write("Diga o valor do depósito: ");
+                bank.Deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write($"Dados da conta: {bank}");
+                Console.Write("\nEntre um valor para depósito: ");
+                bank.Deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write($"Dados da conta atualizados: {bank}");
+                Console.Write("\nDiga um valor para saque (Taxa de R$5,00): ");
+                bank.Withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);  
+                Console.Write($"Dados da conta atualizados: {bank}");
+            }
+            else if (response == "n") {
+                Console.Write($"Dados da conta: {bank}");
+                Console.Write("\nEntre um valor para depósito: ");
+                bank.Deposit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write($"Dados da conta atualizados: {bank}");
+                Console.Write("\nDiga um valor para saque (Taxa de R$5,00): ");
+                bank.Withdraw = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write($"Dados da conta atualizados: {bank}");
+            }
+            else {
+                Console.WriteLine("Erro...");
+                Exercise1();
+            }
         }
     }
 }
