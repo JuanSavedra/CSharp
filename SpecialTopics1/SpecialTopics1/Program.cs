b9b43c6928ad9187ac0a2fc095ca89b2f2081d1b
Juan Savedra <juansavedra827@gmail.com>
2022-05-03 14:09:46 -0300

    var

---
+++ SpecialTopics1/SpecialTopics1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SpecialTopics {
+    class Program {
+        static void Main(string[] args) {
+            Var();
+        }
+
+        static void Var() {
+            /* Variável var
+            Ela se define de acordo com o valor dela 
+            */
+
+            var a = 1; //int
+            var b = 2.00; //double
+            var c = "Nome"; //string
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
