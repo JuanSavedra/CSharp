87f70bab8259fbcaf3e0de1b08b71c54e7daab6d
Juan Savedra <juansavedra827@gmail.com>
2022-05-05 13:00:48 -0300

    exceptions

---
+++ ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace ExceptionHandling {
+    class Program {
+        static void Main(string[] args) {
+            Exception();
+        }
+
+        static void Exception() {
+            /* Exceções
+            Uma exceção é qualquer condição de erro ou comportamento inesperado por um
+            programa em execução
+            No .NET, uma exceção é um objeto herdado da classe System.Exception
+            Quando lançada, uma exceção é propagada na pilha de chamadas de métodos em 
+            execução, até que seja capturada (tratada) ou o programa seja encerrado
+            */
+
+            /* Porque exceções? 
+            O modelo de tratamento de exceções permite que erros sejam tratados de forma
+            consistent ee flexível, usando boas práticas
+            Vantagens: 
+            - Delega a lógica do erro para a classe / método responsável por conhecer as 
+            regras que podem ocasionar o erro
+            - Trata de forma organizada (inclusive hierárquica) exceções de tipos diferentes
+            - A exceção pode carregar dados quaisquer
+            */
+        }
+    }
+}
